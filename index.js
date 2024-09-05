a369b2b31fb600f24d1076af1ccf98e3466763d8
arijitar <arijit.roy155k@gmail.com>
2024-09-06 01:08:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.get('/twitter', (req, res)=> {
+  res.send('arijitar.com')
+})
+
+app.get("/login", (req, res) =>{
+  res.send('<h1>Plese login at chai aur code</h1>')
+})
+app.get('/youtube', (req, res)=>{
+  res.send('<h2>Chai aur code</h2>')
+})
+app.listen(process.env.PORT, ()=> {
+  console.log(`Example app listening on port ${port}`);
+})
